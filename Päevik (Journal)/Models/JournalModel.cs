1313fd16b484ec485e57f354daff0c98387f0fcf
hyowo <hyo@femboy.cx>
2023-05-30 14:55:36 +0300

    add journal model

---
+++ Päevik (Journal)/Models/JournalModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Journal.Models
+{
+    public class JournalModel
+    {
+        public bool IsPrivate { get; set; }
+        public string OriginalPoster { get; set; }
+        public DateTime PostedAt { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
