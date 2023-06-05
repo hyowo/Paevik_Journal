5f6dbab264b712cce93a363e1d0bc524560f916c
hyowo <vilumaaerik@gmail.com>
2023-06-05 23:31:34 +0300

    add a design to newjournal and functionality

--- Päevik (Journal)/Views/NewJournal.xaml.cs
+++ Päevik (Journal)/Views/NewJournal.xaml.cs
@@ -1,3 +1,5 @@
+using Journal.Models;
+
 namespace Journal.Views;
 
 public partial class NewJournal : ContentPage
@@ -6,4 +8,41 @@ public partial class NewJournal : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async Task ShowToast(string message, int durationInSeconds = 3)
+    {
+        ToastMessage.Text = message;
+        ToastContainer.IsVisible = true;
+        await ToastContainer.FadeTo(1, 250);
+        await Task.Delay(durationInSeconds * 1000);
+        await ToastContainer.FadeTo(0, 250);
+        ToastContainer.IsVisible = false;
+    }
+
+    private async void OnSaveClicked(object sender, EventArgs e)
+    {
+        JournalModel model = new()
+        {
+            Title = TitleEntry.Text ?? "",
+            OriginalPoster = NameEntry.Text == string.Empty ? "Anonymous" : NameEntry.Text,
+            PostedAt = DateTime.Now,
+            IsPrivate = IsPrivateCheckbox.IsChecked,
+            Content = ContentEditor.Text ?? ""
+        };
+
+        bool errorToast = false;
+        string message = (model.Title.Length < 5)
+            ? $"Journal title needs to be at least 5 characters long. {(errorToast = true).ToString().Replace("True", "")}"
+            : (model.Content.Length < 20)
+                ? $"Journal content needs to be at least 20 characters long. {(errorToast = true).ToString().Replace("True", "")}"
+                : "Journal posted successfully!";
+        if (errorToast)
+            await ShowToast(message);
+        else
+        {
+            await Application.Current.MainPage.Navigation.PopAsync();
+            if (Application.Current.MainPage is NavigationPage navigationPage && navigationPage.CurrentPage is AllJournals allJournalsPage)
+                await allJournalsPage.ShowToast(message);
+        }
+    }
 }
